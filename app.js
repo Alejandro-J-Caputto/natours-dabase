06ec1e8028572f65d774b992aae9c2efe60ecea5
Alejandro Caputto frias <alejandro_caputto@MacBook-Air-de-Alejandro.local>
2020-12-14 17:43:26 +0100

    security, sanitization, pollution checked

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 //ERROR HANDLING
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController')
@@ -17,13 +20,10 @@ const app = express();
 //MIDDLEWARE STACK
 //SECURITY HTTP HEADERS
 app.use(helmet())
-
 //DEVELOPING LOGGING
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-
-
 //LIMIT REQUEST FROM SAME API
 const limiter = rateLimit({
   max: 100,
@@ -33,6 +33,23 @@ const limiter = rateLimit({
 app.use('/api' , limiter);
 //BODY PARSER READING DATA FROM BODZ INTO REQ.body
 app.use(express.json({limit: '10kb' })); // modifica la data que viene
+
+//DATA SANITIZATION AGAINST NoSQL query injection
+app.use(mongoSanitize());
+//DATA SANITIZATION AGAINST CROSS SITING ATTACKS XXS 
+app.use(xss());
+//PREVENT PARAMETER POLLUTION en este caso con duracion. 
+app.use(hpp({
+  whitelist: [
+    'duration',
+    'ratingsAverage',
+    'maxGroupSize',
+    'ratingsQuantity',
+    'price',
+    'difficulty'
+  ]
+}));
+
 //SERVES STATICS FILES 
 app.use(express.static(`${__dirname}/public`));
 
